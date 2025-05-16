9fe3ab6aba7f511cb47ad375c33a5a0554f969d5
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-16 17:14:26 +0530

    Write recognized speech to text file and confirm completion

--- audio_To_text.py
+++ audio_To_text.py
@@ -11,3 +11,8 @@ with audio_file as source:
      r.adjust_for_ambient_noise(source)
      audio = r.record(source)
 result = r.recognize_google(audio)
+with open("test.txt",mode ="w") as file:
+     file.write("Recognized text:")
+     file.write("\n")
+     file.write(result)
+     print("Hurray! conversion is completed")
