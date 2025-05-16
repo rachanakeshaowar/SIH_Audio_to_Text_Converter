46e43080d4a78c943521a0504244d1cb2fcbf58e
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-16 09:44:16 +0530

    Created changeOnHover function to dynamically change button background color on hover

--- Project_SIH/reactTotext.py
+++ Project_SIH/reactTotext.py
@@ -9,3 +9,7 @@ from datetime import date
 import webbrowser
 import random
 def changeOnHover(button, colorOnHover, colorOnLeave):
+        button.bind("<Enter>", func=lambda e: button.config(
+            background=colorOnHover))
+        button.bind("<Leave>", func=lambda e: button.config(
+            background=colorOnLeave))
