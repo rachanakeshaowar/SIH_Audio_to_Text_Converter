0b2f4ca097f1e31f7f28f5ab303ec0143252616e
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-16 16:21:50 +0530

    Position file explorer label in the GUI grid layout

--- reactTotext.py
+++ reactTotext.py
@@ -109,6 +109,7 @@ if __name__ == "__main__" :
     root.title("Text-Recording")
     filename =''
     label_file_explorer = Label(root,text = "No file Chosen", width = 12, height = 1,fg = "blue")
+    label_file_explorer.grid(column = 0, row = 0)
 
         
         
