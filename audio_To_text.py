6514cc13c36019e48da9d4627ea33081347de056
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-16 17:18:00 +0530

    Create and initialize text file for saving recognized speech

--- audio_To_text.py
+++ audio_To_text.py
@@ -23,4 +23,7 @@ with audio_file as source:
   r.adjust_for_ambient_noise(source)
   audio = r.record(source)
 result = r.recognize_google(audio)
+with open("test.txt",mode ="w") as file:
+    file.write("Recognized text:")
+    file.write("\n")
  
