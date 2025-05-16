b80025370aa3f5dc9d9b3a8e79b63f1b6d99c3dd
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-16 17:13:40 +0530

    Process audio file and transcribe using Google Speech Recognition

--- audio_To_text.py
+++ audio_To_text.py
@@ -4,3 +4,10 @@ import speech_recognition as sr
 import os
 import datetime
 r = sr.Recognizer()
+
+
+audio_file = sr.AudioFile("download.wav")
+with audio_file as source:
+     r.adjust_for_ambient_noise(source)
+     audio = r.record(source)
+result = r.recognize_google(audio)
