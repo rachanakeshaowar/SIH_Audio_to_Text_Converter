64980ff7913f5a8b48da6d0c4ec8d78135cbcfde
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-17 13:57:54 +0530

    feat: add support for Bahasa Melayu, Català, Čeština, and Deutsch with respective locale codes

--- Project_SIH/language.js
+++ Project_SIH/language.js
@@ -1,3 +1,7 @@
 var langs =
 [['Afrikaans',       ['af-ZA']],
  ['Bahasa Indonesia',['id-ID']],
+ ['Bahasa Melayu',   ['ms-MY']],
+ ['Català',          ['ca-ES']],
+ ['Čeština',         ['cs-CZ']],
+ ['Deutsch',         ['de-DE']],
