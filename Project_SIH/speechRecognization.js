505e7ed4c40e524ad56338ec020cfb55d5a4168a
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-17 17:34:51 +0530

    Add file download functionality with dynamic filename and content

--- Project_SIH/speechRecognization.js
+++ Project_SIH/speechRecognization.js
@@ -67,13 +67,13 @@ function downloadFile(filename, content) {
        const content = document.getElementById('final').value;
 
        if (filename && content) {
-
+           downloadFile(filename + '.txt', content);
 
 
       
 
 
-        
+
 
 
   
