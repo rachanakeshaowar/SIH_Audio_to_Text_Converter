4ac4d2668817bbd9a7a8f52d33d7db852cf42b82
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-17 14:22:55 +0530

    feat: enable continuous speech recognition and add speech flag

--- Project_SIH/speechRecognization.js
+++ Project_SIH/speechRecognization.js
@@ -2,3 +2,5 @@ if ("webkitSpeechRecognition" in window) {
   let speechRecognition = new 
   webkitSpeechRecognition();
   let final_transcript = "";
+  var speech = true;
+  speechRecognition.continuous = true;
